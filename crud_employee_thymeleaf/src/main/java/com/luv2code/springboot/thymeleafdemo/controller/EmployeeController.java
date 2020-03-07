dca9396d2a298c73bc2d1c174547ec45378c81b2
patrickkabongo <cracciska@gmail.com>
2020-03-07 16:02:33 -0300

    Get Employees from Database

---
+++ crud_employee_thymeleaf/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {	
+	
+	private EmployeeService employeeService;
+	
+	@Autowired	
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+
+	@GetMapping("/list")
+	public String getEmployees(Model theModel) {
+		
+		List<Employee> theEmployees;
+		
+		theEmployees = employeeService.findAll();
+		
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+
+}
